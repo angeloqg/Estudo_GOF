0d05ec6fbce7c2d10f5790e621fc6767c6579c2f
angeloqg <angeloqg@hotmail.com>
2022-11-08 14:16:18 -0300

    Criational Factory - Organização inicial dos projetos

---
+++ src/Criational_Patterns/Factory_Method/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Factory_Method
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Criational_Patterns/Simple_Factory/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Simple_Factory
+{
+    public class Class1
+    {
+    }
+}
