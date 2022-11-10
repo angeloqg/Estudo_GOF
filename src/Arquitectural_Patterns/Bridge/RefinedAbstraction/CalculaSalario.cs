5f109a4eea9853b2713ff8a19c4a1943e5b74eb4
angeloqg <angeloqg@hotmail.com>
2022-11-10 17:40:25 -0300

    Arquitectural Pattern - Bridge (Ajuste)

--- src/Arquitectural_Patterns/Bridge/RefinedAbstraction/CalculaSalario.cs
+++ src/Arquitectural_Patterns/Bridge/RefinedAbstraction/CalculaSalario.cs
@@ -2,10 +2,6 @@
 using Bridge.Domain;
 using Bridge.Implementor;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Bridge.RefinedAbstraction
 {
